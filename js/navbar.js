13d66f69b04e689876b21e4132c82d9f9a22138a
xiang1224 <a10083237@gmail.com>
2023-07-21 11:38:05 +0800

    20230721-1

---
+++ js/navbar.js
@@ -0,0 +1,48 @@
+class MyHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+    <header>
+        <nav class="navbar navbar-expand-lg ">
+            <a class="navbar-brand" href="#">東方改裝車廠</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">最新消息</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownCars" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            車種分類
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownCars">
+                            <li><a class="dropdown-item" href="#">跑車</a></li>
+                            <li><a class="dropdown-item" href="#">轎車</a></li>
+                            <li><a class="dropdown-item" href="#">機車</a></li>
+                            <!-- Add more items here if needed -->
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownAbout" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            關於我們
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownAbout">
+                            <li><a class="dropdown-item" href="#">團隊介紹</a></li>
+                            <li><a class="dropdown-item" href="#">公司歷程</a></li>
+                            <li><a class="dropdown-item" href="#">服務宗旨</a></li>
+                            <!-- Add more items here if needed -->
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>`;
+    }
+}
+
+customElements.define('my-header', MyHeader)
+
