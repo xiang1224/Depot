d4964d95f773e2dbe6053cf0c86f754e8a3e1f29
xiang1224 <a10083237@gmail.com>
2023-07-26 00:44:03 +0800

    20230726-a

--- js/price.js
+++ js/price.js
@@ -12,14 +12,20 @@ function calculateTotal() {
     const engineSelect = document.getElementById('engineSelect');
     const brakeSelect = document.getElementById('brakeSelect');
     const suspensionSelect = document.getElementById('suspensionSelect');
+    const turboSelect = document.getElementById('turboSelect');
+    const bodySelect = document.getElementById('bodySelect');
+    const transmissionSelect = document.getElementById('transmissionSelect');
 
-    // 獲取引擎、剎車和懸吊的價格
+    // 獲取引擎、剎車、懸吊、渦輪、板金和變速箱的價格
     const enginePrice = parseInt(engineSelect.value);
     const brakePrice = parseInt(brakeSelect.value);
     const suspensionPrice = parseInt(suspensionSelect.value);
+    const turboPrice = parseInt(turboSelect.value);
+    const bodyPrice = parseInt(bodySelect.value);
+    const transmissionPrice = parseInt(transmissionSelect.value);
 
     // 將選擇的價格加入總金額
-    total += enginePrice + brakePrice + suspensionPrice;
+    total += enginePrice + brakePrice + suspensionPrice + turboPrice + bodyPrice + transmissionPrice;
 
     document.getElementById('total').innerText = total;
 }
