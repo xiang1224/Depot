abec681a6321540f84c9a4e9efb4070a047c9b5d
xiang1224 <a10083237@gmail.com>
2023-07-26 17:06:30 +0800

    20230726-B

--- js/price.js
+++ js/price.js
@@ -1,11 +1,28 @@
+function toggleSubItems(subItemsId) {
+    const subItems = document.getElementById(subItemsId);
+    if (subItems.style.display === 'block') {
+        subItems.style.display = 'none';
+    } else {
+        subItems.style.display = 'block';
+    }
+}
+
 function calculateTotal() {
     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    const radios = document.querySelectorAll('input[type="radio"]');
     let total = 0;
+    let tirePrice = 0; // 輪胎價格
 
     checkboxes.forEach(checkbox => {
-        if (checkbox.checked) {
+        if (checkbox.checked && checkbox.id !== 'item12') {
             const price = parseInt(checkbox.dataset.price);
             total += price;
+        }
+    });
+
+    radios.forEach(radio => {
+        if (radio.checked && radio.name === 'tireType') {
+            tirePrice = parseInt(radio.dataset.price);
         }
     });
 
@@ -24,8 +41,22 @@ function calculateTotal() {
     const bodyPrice = parseInt(bodySelect.value);
     const transmissionPrice = parseInt(transmissionSelect.value);
 
-    // 將選擇的價格加入總金額
-    total += enginePrice + brakePrice + suspensionPrice + turboPrice + bodyPrice + transmissionPrice;
+    // 將選擇的價格加入總金額，包括輪胎價格，除非取消輪胎選項
+    total += enginePrice + brakePrice + suspensionPrice + turboPrice + bodyPrice + transmissionPrice + tirePrice;
 
     document.getElementById('total').innerText = total;
+}
+
+function toggleSubItems(subItemsId) {
+    const subItems = document.getElementById(subItemsId);
+    subItems.style.display = !subItems.style.display || subItems.style.display === 'none' ? 'block' : 'none';
+
+    // 取消輪胎選項後，重設賽車型和休旅車型的 radio 選項
+    if (subItemsId === 'subItems1' && subItems.style.display === 'none') {
+        const radios = document.querySelectorAll('input[name="tireType"]');
+        radios.forEach(radio => {
+            radio.checked = false;
+        });
+        calculateTotal(); // 更新價格
+    }
 }
