22928a20ae8b5a754170edfcd90ab5e9ec049695
xiang1224 <a10083237@gmail.com>
2023-08-19 15:55:19 +0800

    20230819-2

--- js/navbar.js
+++ js/navbar.js
@@ -3,7 +3,7 @@ class MyHeader extends HTMLElement {
         this.innerHTML = `
     <header>
         <nav class="navbar navbar-expand-lg ">
-            <a class="navbar-brand" href="index.html"><img src="img/marlin-LOGO.png" style="height: 110px;"></a>
+            <a class="navbar-brand" href="index.html"><img src="" style="height: 110px;"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
